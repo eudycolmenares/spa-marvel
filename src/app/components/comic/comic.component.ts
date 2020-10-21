6d90c236ef65d8a980154f40657fba6414a21d50
eudy <eudy.developer@gmail.com>
2020-10-21 13:09:00 -0500

    Componente Comic: maquetacion inicial

--- src/app/components/comic/comic.component.ts
+++ src/app/components/comic/comic.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './comic.component.html',
   styleUrls: ['./comic.component.scss']
 })
+
 export class ComicComponent implements OnInit {
 
   constructor() { }
--- src/app/pages/aside/aside.component.ts
+++ src/app/pages/aside/aside.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-aside',
   templateUrl: './aside.component.html',
   styleUrls: ['./aside.component.scss']
 })
+
 export class AsideComponent implements OnInit {
+  @ViewChild('aside') aside: ElementRef;
+  @ViewChild('title') title: ElementRef;
+  @ViewChild('comics') comics: ElementRef;
+  arrayComics = [];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+    const asideH = this.aside.nativeElement.offsetHeight;
+    const titleH = this.title.nativeElement.offsetHeight;
+    this.comics.nativeElement.style.height = (asideH - titleH - 32 - 32) + 'px';
+    this.arrayComics = [1,2,3,4,5,6,7];
+  }
 }
