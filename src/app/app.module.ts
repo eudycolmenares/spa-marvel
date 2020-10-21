b954f915a255cf237c97614d0439c6670786bcb2
eudy <eudy.developer@gmail.com>
2020-10-21 10:21:06 -0500

    Componente Sidebar: maquetacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MainComponent } from './pages/main/main.component';
 import { CharacterComponent } from './components/character/character.component';
+import { AsideComponent } from './pages/aside/aside.component';
+import { ComicComponent } from './components/comic/comic.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +14,8 @@ import { CharacterComponent } from './components/character/character.component';
     HeaderComponent,
     MainComponent,
     CharacterComponent,
+    AsideComponent,
+    ComicComponent,
   ],
   imports: [
     BrowserModule
